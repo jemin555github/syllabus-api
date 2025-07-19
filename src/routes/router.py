7cf3ee4434a118b2f9ad125f9542304f7ec99a0d
jemin <jemin.ajudiya@mindinventory.com>
2025-07-20 00:27:56 +0530

    removed code of excel update

--- src/routes/router.py
+++ src/routes/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from src.api.v1.auth.route import auth_router
 from src.api.v1.medias.route import media_router
-from src.api.v1.excel_update.route import sheet_router
 
 
 router = APIRouter(prefix="/api/v1/dpp-app")
@@ -9,5 +8,4 @@ router = APIRouter(prefix="/api/v1/dpp-app")
 router.include_router(auth_router, tags=['Auth'])
 router.include_router(media_router, tags=['Media'])
 
-router.include_router(sheet_router, tags=['Excel'])
 
